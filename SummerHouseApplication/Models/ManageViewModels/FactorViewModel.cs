18ce6140f5b7d78de4afdf5a9d6774c9c5a8ecb9
Santeri Nousiainen <santeri.nousiainen@ecraft.com>
2017-05-25 21:12:11 +0300

    Changed project to pure Asp.net core mvc for now. vue with google maps was too much for now.

---
+++ SummerHouseApplication/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SummerHouseApplication.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
